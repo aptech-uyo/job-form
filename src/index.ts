c447760f8ee8eef02f5716e12ef4e2c62fc8c424
Bennet Otoho <bennet2000@outlook.com>
2024-09-20 16:04:17 +0100

    set marketing lead

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { FormData } from "./model"
-
 import { fillSpreadsheetDetails, fillSpreadsheetOverview, saveFiles } from "./persist-form-entry"
 
-export function doGet(_e: GoogleAppsScript.Events.DoGet): GoogleAppsScript.HTML.HtmlOutput {
+export let formHearAbout: string
+
+export function doGet(e: GoogleAppsScript.Events.DoGet): GoogleAppsScript.HTML.HtmlOutput {
   // form.html
+  formHearAbout = e.parameter.formHearAbout
   return HtmlService.createTemplateFromFile("form")
     .evaluate()
     .setTitle("Job Application for Graduate Management Trainee, Solution Tech Limited")
@@ -12,7 +14,9 @@ export function doGet(_e: GoogleAppsScript.Events.DoGet): GoogleAppsScript.HTML.
 }
 
 export function include(fileName: string): string {
-  return HtmlService.createHtmlOutputFromFile(fileName).getContent()
+  const template = HtmlService.createTemplateFromFile(fileName)
+  if (fileName.startsWith("javascript")) template.formHearAbout = formHearAbout
+  return template.evaluate().getContent()
 }
 
 export function registerSubmission(data: FormData): boolean {
